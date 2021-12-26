29df16570e5f5413ab4d0fd596a2ad3f5c35bb14
mshahidul <alam30nov@gmail.com>
2021-12-26 13:28:26 +0600

    Shahidul

--- app/src/main/java/org/icddrb/ibd/LoginActivity.java
+++ app/src/main/java/org/icddrb/ibd/LoginActivity.java
@@ -129,7 +129,7 @@ public class LoginActivity extends Activity{
             //Need to update date every time whenever shared updated system
             //Format: DDMMYYYY
             //*********************************************************************
-            SystemUpdateDT = "23102021";
+            SystemUpdateDT = "15122021";
             lblSystemDate.setText("Version:1.0, Built on: " + SystemUpdateDT + "(" + Global.Organization + ")");
 
             //Check for Internet connectivity
